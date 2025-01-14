195a931e5f61b32242904f863614cb34b63a3e29
Zac <a19860101@gmail.com>
2025-01-14 19:27:25 +0800

    20250114

--- app/Filament/Resources/CategoryResource/RelationManagers/ProductsRelationManager.php
+++ app/Filament/Resources/CategoryResource/RelationManagers/ProductsRelationManager.php
@@ -10,6 +10,8 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use App\Enums\ProductStatus;
+
 class ProductsRelationManager extends RelationManager
 {
     protected static string $relationship = 'products';
@@ -19,8 +21,35 @@ class ProductsRelationManager extends RelationManager
         return $form
             ->schema([
                 Forms\Components\TextInput::make('title')
+                    ->label('商品名稱')
                     ->required()
                     ->maxLength(255),
+                Forms\Components\RichEditor::make('description')
+                    ->label('商品描述')
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('cover')
+                    ->label('商品封面')
+                    ->required()
+                    ,
+                Forms\Components\FileUpload::make('gallery')
+                    ->label('圖片集')
+                    ->multiple()
+                    ->reorderable()
+                    ,
+                Forms\Components\TextInput::make('price')
+                    ->label('價格')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('sale')
+                    ->label('原價')
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('published_at')
+                    ->label('上架日期'),
+                Forms\Components\Select::make('status')
+                    ->label('商品狀態')
+                    ->required()
+                    ->enum(ProductStatus::class)
+                    ->options(ProductStatus::class),
             ]);
     }
 
